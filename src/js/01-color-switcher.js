499a82e6f031c6e8d5628ffddde3c8c3d728493a
Jlach2000 <joanna.lach@vp.pl>
2024-03-06 21:07:25 +0100

    321

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
 
+function getRandomHexColor() {  
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    }
+
+    let intervalId;
+
+function changeBackgroundColor() {
+    document.body.style.backgroundColor = getRandomHexColor();
+}
+
+document.querySelector('[data-start]').addEventListener('click',function() {
+   this.disabled = true;
+   document.querySelector('[data-stop]').disabled = false;
+   intervalId = setInterval(changeBackgroundColor, 1000);
+});
+
+document.querySelector('[data-stop]').addEventListener('click',function() {
+this.disabled = true;
+document.querySelector('[data-start]').disabled = false;
+clearInterval(intervalId);
+});
+
+
