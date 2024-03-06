28449c0b70c09a4e632ad3fa0faccf5b6370a7de
Jlach2000 <143550355+Jlach2000@users.noreply.github.com>
2024-03-06 20:08:00 +0000

    Deploying to gh-pages from @ Jlach2000/goit-js-hw-09@499a82e6f031c6e8d5628ffddde3c8c3d728493a 🚀

---
+++ 01-color-switcher.749f2d45.js
@@ -0,0 +1,2 @@
+let t;function e(){document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}document.querySelector("[data-start]").addEventListener("click",(function(){this.disabled=!0,document.querySelector("[data-stop]").disabled=!1,t=setInterval(e,1e3)})),document.querySelector("[data-stop]").addEventListener("click",(function(){this.disabled=!0,document.querySelector("[data-start]").disabled=!1,clearInterval(t)}));
+//# sourceMappingURL=01-color-switcher.749f2d45.js.map
